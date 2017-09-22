6f8881b6060b4ddf8e3f0a3aaf3708768480c655
ValBrynn <guardianofthecity34@gmail.com>
2017-09-22 17:37:07 +0200

    Changes in Book , CB ...

--- Lab3-B/src/lab3/b/CollectionOfBooks.java
+++ Lab3-B/src/lab3/b/CollectionOfBooks.java
@@ -5,10 +5,51 @@
  */
 package lab3.b;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  *
  * @author FarHad
  */
 public class CollectionOfBooks {
+    
+    private Book book;
+    private ArrayList<Book> books;
+            
+//    public CollectionOfBooks(Book book){
+//    
+//      books.add(book);
+//    }
+    
+    public CollectionOfBooks(ArrayList<Book> addedBooks)
+    {
+        books=addedBooks;
+    }
+    
+    public int getNrOfBooks()
+    {
+       return books.size();
+    }
+            
+    public void addBook(Book book)
+    {
+      books.add(book);
+    }
+    
+    public List<Book> getBooksByTitle(String title)
+    {
+        return (ArrayList<Book>) books.clone();
+    }
+    
+    
+    public String toString()
+    {
+        
+        String info =" "+books;
+        
+        return info;
+    
+    }
     
 }
